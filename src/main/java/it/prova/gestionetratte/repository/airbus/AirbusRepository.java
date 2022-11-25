feed9feae479c8a7bad3b6d1e84ee6870a850d72
Balraj1108 <balrajsingh1108@gmail.com>
2022-11-25 15:45:41 +0100

    Implementazione Repository

--- src/main/java/it/prova/gestionetratte/repository/airbus/AirbusRepository.java
+++ src/main/java/it/prova/gestionetratte/repository/airbus/AirbusRepository.java
@@ -1,5 +1,17 @@
 package it.prova.gestionetratte.repository.airbus;
 
-public interface AirbusRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public interface AirbusRepository extends CrudRepository<Airbus, Long> {
+
+	@Query("select distinct a from Airbus a left join fetch a.tratte ")
+	List<Airbus> findAllEager();
+
+	@Query("from Airbus a left join fetch a.tratte where a.id=?1")
+	Airbus findByIdEager(Long id);
 }
