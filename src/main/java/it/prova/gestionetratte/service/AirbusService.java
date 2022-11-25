05c338033c6d8cc37991102ffb0b66ac6875cac1
Balraj1108 <balrajsingh1108@gmail.com>
2022-11-25 15:56:02 +0100

    Creazione Service

---
+++ src/main/java/it/prova/gestionetratte/service/AirbusService.java
@@ -0,0 +1,22 @@
+package it.prova.gestionetratte.service;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public interface AirbusService {
+
+	List<Airbus> listAllElements();
+	
+	List<Airbus> listAllElementsEager();
+
+	Airbus caricaSingoloElemento(Long id);
+	
+	Airbus caricaSingoloElementoConTratte(Long id);
+
+	Airbus aggiorna(Airbus airbusInstance);
+
+	Airbus inserisciNuovo(Airbus airbusInstance);
+
+	void rimuovi(Long id);
+}
